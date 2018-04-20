dd6b016f7ecd01775553cbc9fef0f70011bf415f
TehSeph <kazuhiroichi@gmail.com>
2018-04-19 22:22:52 -0500

    Update links

--- src/main/java/tehseph/netherfoundation/Reference.java
+++ src/main/java/tehseph/netherfoundation/Reference.java
@@ -20,7 +20,7 @@ public class Reference {
     public static final String VERSION            = "0.1.0a";
     public static final String MC_VERSIONS        = "[1.12.2, 1.13)";
 
-    public static final String UPDATE_JSON        = "https://raw.githubusercontent.com/TehSeph/NetherFoundation/master/update.json";
+    public static final String UPDATE_JSON        = "https://github.com/TehSeph/NetherFoundation/raw/master/update.json";
 
     public static final String GUI_FACTORY        = "tehseph.netherfoundation.client.gui.ConfigGuiFactory";
     public static final String CLIENT_PROXY       = "tehseph.netherfoundation.proxy.ClientProxy";
