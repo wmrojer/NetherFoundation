85ca10a98c9e2f5fbbec6ed8f251bcc30910edb0
TehSeph <kazuhiroichi@gmail.com>
2018-06-07 15:05:40 -0500

    version bump

--- src/main/java/tehseph/netherfoundation/Reference.java
+++ src/main/java/tehseph/netherfoundation/Reference.java
@@ -14,7 +14,7 @@ public class Reference {
 
     public static final String MOD_ID             = "netherfoundation";
     public static final String NAME               = "Nether Foundation";
-    public static final String VERSION            = "1.1.3";
+    public static final String VERSION            = "1.2.0";
     public static final String MC_VERSIONS        = "[1.12.2, 1.13)";
 
     public static final String UPDATE_JSON        = "https://github.com/TehSeph/NetherFoundation/raw/master/update.json";
